d2ec99ca3ef7a82fdffec4b0c60b19fc4a743fed
heitormaca <heitor_soares@live.com>
2019-12-03 14:46:06 -0300

    inserindo novo react

--- tw_react/src/App.js
+++ tw_react/src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './assets/css/flexbox.css';
-import './assets/css/reset.css';
-import './assets/css/style.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
 
 function App() {
   return (
