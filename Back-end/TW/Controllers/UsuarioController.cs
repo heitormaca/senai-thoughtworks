8ebfa18527e750ebcf6bed762d2c56f9bfe3fd10
heitormaca <heitor_soares@live.com>
2019-12-09 11:34:55 -0300

    esqueci a senha funcionando sem email

--- Back-end/TW/Controllers/UsuarioController.cs
+++ Back-end/TW/Controllers/UsuarioController.cs
@@ -76,7 +76,6 @@ namespace TW.Controllers
                 return StatusCode(500, e);
             }
         }
-        [Authorize]
         [HttpPatch("forgotPassword")]
         public async Task<IActionResult> PostForgotPassword([FromBody] ForgotPasswordViewModel verificacao)
         {
@@ -84,10 +83,13 @@ namespace TW.Controllers
             var user = Autenticacao(verificacao);
             if(user!=null)
             {   
+                
                 await repositorio.PutNewPassword(user);
+                
+                
                 return Ok(user);
              
-                // EnviaEmailcomNovaSenha()
+                
 
             }else{
                 return response;
@@ -99,29 +101,7 @@ namespace TW.Controllers
             return usuario;
         }
 
-        public static string Encrypt(string encryptString)    
-        {    
-            string EncryptionKey = "0ram@1234xxxxxxxxxxtttttuuuuuiiiiio";  //we can change the code converstion key as per our requirement    
-            byte[] clearBytes = Encoding.Unicode.GetBytes(encryptString);    
-            using (Aes encryptor = Aes.Create())    
-            {    
-                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] {      
-            0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76      
-        });    
-                encryptor.Key = pdb.GetBytes(32);    
-                encryptor.IV = pdb.GetBytes(16);    
-                using (MemoryStream ms = new MemoryStream())    
-                {    
-                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))    
-                    {    
-                        cs.Write(clearBytes, 0, clearBytes.Length);    
-                        cs.Close();    
-                    }    
-                    encryptString = Convert.ToBase64String(ms.ToArray());    
-                }    
-            }    
-            return encryptString;    
-        }    
+            
         /// <summary>
         /// Método para cadastrar usuário comum ou administrador no sistema.
         /// </summary>
@@ -189,6 +169,29 @@ namespace TW.Controllers
             } else {
                 return null;
             }           
+        }
+        public static string Encrypt(string encryptString)    
+        {    
+            string EncryptionKey = "0ram@1234xxxxxxxxxxtttttuuuuuiiiiio";  //we can change the code converstion key as per our requirement    
+            byte[] clearBytes = Encoding.Unicode.GetBytes(encryptString);    
+            using (Aes encryptor = Aes.Create())    
+            {    
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] {      
+            0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76      
+        });    
+                encryptor.Key = pdb.GetBytes(32);    
+                encryptor.IV = pdb.GetBytes(16);    
+                using (MemoryStream ms = new MemoryStream())    
+                {    
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))    
+                    {    
+                        cs.Write(clearBytes, 0, clearBytes.Length);    
+                        cs.Close();    
+                    }    
+                    encryptString = Convert.ToBase64String(ms.ToArray());    
+                }    
+            }    
+            return encryptString;    
         }
     }
 }
